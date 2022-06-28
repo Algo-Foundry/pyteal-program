25213c7c03178ae29bd6b164ce847ffab63a17fb
Charlie Kong <charlie@undercurrent.tech>
2022-06-28 16:07:35 +0800

    solution code

--- assets/pyteal_program.py
+++ assets/pyteal_program.py
@@ -1,5 +1,3 @@
-# Contract Account
-
 # Add directory to path so that algobpy can be imported
 import sys
 sys.path.insert(0,'.')
@@ -9,12 +7,31 @@ from pyteal import *
 
 def main(RECEIVER_1, RECEIVER_2):
 
-    # Write your code here
-    program = Return(Int(0))
+    receiver_1_checks = And(
+        Arg(0) == Bytes("rcv1password"), #rcv1 password
+        Txn.amount() <= Int(5000000) #5 Algos 
+    )
+
+    receiver_2_checks = And(
+        Arg(0) == Bytes("rcv2password"), #rcv2 password
+        Txn.amount() <= Int(10000000) #10 Algos 
+    )
+
+    receiver_checks = Cond(
+        [Txn.receiver() == Addr(RECEIVER_1), receiver_1_checks],
+        [Txn.receiver() == Addr(RECEIVER_2), receiver_2_checks]
+    )
+
+    program = And(
+        Txn.rekey_to() == Global.zero_address(),
+        Txn.close_remainder_to() == Global.zero_address(),
+        receiver_checks
+    )
 
     return program
 
 if __name__ == "__main__":
+    # Default receiver address used if params are not supplied when deploying this contract
     params = {
         "RECEIVER_1": "R4VDREHBHVETKRPBZT6IDOQQL4FBHLBYQBQQJPIBXLTCVXYJX7Z5WLDSZY",
         "RECEIVER_2": "WRBVLPUHQZ5O2UIZAKYKKMOUSNPOFIL6ALUZQZLHBDUSIKXHAEEIELWBFQ",
