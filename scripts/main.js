f14da49a6c4285f59fe18580bf0a977371804780
Charlie Kong <charlie@undercurrent.tech>
2023-01-20 16:55:22 +0800

    fix typo

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ async function run (runtimeEnv, deployer) {
 
   // supply params to program during compilation - not the same as passing arguments
   const templateParams = {
-    RECEVIER_1: acc1.addr,
+    RECEIVER_1: acc1.addr,
     RECEIVER_2: acc2.addr
   }
   
